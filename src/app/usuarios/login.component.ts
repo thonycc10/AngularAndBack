ddd69473a8d7aa9aaf2c8c31b842f66463d0d630
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-02-21 19:04:36 -0500

    AC: añadiendo class typescript usuario

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Usuario} from './usuario';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = 'Por favor Sign In!';
-
-  constructor() { }
+  usuario: Usuario;
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
+  }
+
+
+  login(): void {
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.password == null) {
+      swal('Error Login', 'Username o password vacias!', 'error');
+      return;
+    }
   }
 
 }
