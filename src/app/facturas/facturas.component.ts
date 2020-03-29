e1949e6e1af219cd10ca7076b8081491e0c5470c
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-03-29 01:20:59 -0500

    añadiendo metodos de si existe repetido en una list

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -7,6 +7,8 @@ import {FormControl} from '@angular/forms';
 import {flatMap, map, startWith} from 'rxjs/operators';
 import {FacturaService} from './services/factura.service';
 import {Producto} from './models/producto';
+import {MatAutocomplete, MatAutocompleteSelectedEvent} from '@angular/material';
+import {ItemFactura} from './models/item-factura';
 
 @Component({
   selector: 'app-facturas',
@@ -46,4 +48,50 @@ export class FacturasComponent implements OnInit {
   mostrarNombre(producto?: Producto): string | undefined {
     return producto ? producto.nombre : undefined;
   }
+
+  seleccionProducto(event: MatAutocompleteSelectedEvent): void {
+    const producto = event.option.value as Producto; // se convierte en objeto generico en un producto
+    console.log(producto);
+    if (this.existeItem(producto.id)) {
+      this.incrementaCantidad(producto.id);
+    } else {
+      const nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.itemFacturaList.push(nuevoItem);
+    }
+    this.autocompleteControl.setValue('');
+    event.option.focus();
+    event.option.deselect();
+  }
+
+  actualizarCantidad(id: number, event: any): void {
+    const cantidad: number = event.target.value as number;
+    // map se usa para modificar
+    this.factura.itemFacturaList = this.factura.itemFacturaList.map((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
+
+  existeItem(id: number): boolean {
+    let existe = false;
+    this.factura.itemFacturaList.forEach((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        existe = true;
+      }
+    });
+    return  existe;
+  }
+
+  incrementaCantidad(id: number): void {
+    this.factura.itemFacturaList = this.factura.itemFacturaList.map((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        ++item.cantidad;
+      }
+      return  item;
+    });
+  }
+
 }
